06ecefaaa4e55eb7254ef855e6160cee0564a8db
hpham474 <hpham474@gmail.com>
2024-10-30 04:44:13 -0500

    display sample messages in index route

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
+
 const newRouter = require("./routes/newRouter");
 const indexRouter = require("./routes/indexRouter");
 
-app.use("/new", (req, res) => res.send("New route"));
-app.use("/", (req, res) => res.send("Index route"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/new", newRouter);
+app.use("/", indexRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
