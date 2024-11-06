ec906ec90760fd6646e8b62b60aeb7de62683f20
hpham474 <hpham474@gmail.com>
2024-11-06 04:40:22 -0600

    create form template

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require("node:path");
 const newRouter = require("./routes/newRouter");
 const indexRouter = require("./routes/indexRouter");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
--- routes/newRouter.js
+++ routes/newRouter.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("New Route"));
+indexRouter.get("/", (req, res) => {
+  res.render("form");
+});
 
 module.exports = indexRouter;
